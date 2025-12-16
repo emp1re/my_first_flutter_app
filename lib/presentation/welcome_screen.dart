23739af7e2be04c35984eceae363891286aa00ef
v.seleckij <v.seleckij@gmail.com>
2025-12-16 11:55:34 +0200

    random dice

--- lib/presentation/welcome_screen.dart
+++ lib/presentation/welcome_screen.dart
@@ -1,25 +1,74 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+
+Map<int, String> diceMap = {
+  0: 'assets/images/1_dot.png',
+  1: 'assets/images/2_dots.png',
+  2: 'assets/images/3_dots.png',
+  3: 'assets/images/4_dots.png',
+  4: 'assets/images/5_dots.png',
+  5: 'assets/images/6_dots.png',
+};
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final rand = Random(context.hashCode);
+    int num = 0;
     return Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topRight,
-              end: Alignment.bottomLeft,
-              colors: [
-                Colors.yellow,
-                Colors.red,
-                Colors.blue,
-                ],
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          begin: Alignment.topRight,
+          end: Alignment.bottomLeft,
+          colors: [Colors.yellow, Colors.red, Colors.blue],
+        ),
+      ),
+      child: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              style: TextStyle(fontSize: 24, color: Colors.white),
+              'Hello, Flutter!',
             ),
+            RandomDice()
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class RandomDice extends StatefulWidget {
+  const RandomDice({super.key});
+  @override
+  State<RandomDice> createState() => _RandomDiceState();
+}
+
+class _RandomDiceState extends State<RandomDice> {
+  final rand = Random();
+  int num = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.only(top: 20),
+      child: Column(
+        children: [
+          Image.asset(diceMap[num]!, width: 150, height: 150),
+          FloatingActionButton(
+            onPressed: () {
+              setState(() {
+                num = rand.nextInt(6);
+              });
+            },
+            child: const Icon(Icons.casino),
           ),
-          child: Center(child: Text(
-            style: TextStyle(fontSize: 24, color: Colors.white),
-            'Hello, Flutter!')),
-        );
+        ],
+      ),
+    );
   }
 }
